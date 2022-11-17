6a953e55a0213928f4d2574fbe104af8642fac87
davodyadegari <davoud.yadegari88@gmail.com>
2022-11-17 10:49:17 +0330

    second commit

--- src/Exeptions/InvalidAmountException.java
+++
@@ -1,8 +0,0 @@
-package Exeptions;
-
-public class InvalidAmountException extends RuntimeException {
-    public static final String Message = "That's too much!";
-    public InvalidAmountException() {
-        super(Message);
-    }
-}
