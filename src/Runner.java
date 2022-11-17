8b4d03f7f12f16314e0df4227b0650a084466f28
davodyadegari <davoud.yadegari88@gmail.com>
2022-11-17 10:42:40 +0330

    first commit

---
+++ src/Runner.java
@@ -0,0 +1,13 @@
+import controller.ATM;
+
+import java.io.IOException;
+
+public class Runner {
+
+    public static void main(String[] args) throws IOException {
+
+        ATM atm = new ATM();
+        atm.run();
+
+    }
+}
